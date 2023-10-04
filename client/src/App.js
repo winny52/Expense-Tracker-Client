68db43420beeb3d0ea4a48727e9fb5b2e1a08303
tony-brian <btoni3823@gmail.com>
2023-10-04 15:39:51 +0300

    complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
-import ExpenseForm from "./components/ExpenseForm";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import LandingPage from './components/LandingPage';
+import LoginForm from './components/LoginForm';
+import SignUpForm from './components/SignUpForm';
+import ContactPage from './components/ContactPage'; // 
 
 function App() {
   return (
-    <div className="App bg-blue-100">
-      <ExpenseForm />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/get-started" element={<SignUpForm />} />
+          <Route path="/contact" element={<ContactPage />} /> 
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
