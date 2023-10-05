b4e777eb7598b366b7c1b3700ffdb73a19fd8667
RickMohamedFiri <rick.mohamed@student.moringaschool.com>
2023-10-05 10:48:33 +0300

    added padding to the landing page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className="fixed top-0 right-0 p-4">
-      <ul className="flex space-x-4">
+    <nav className="m-5 fixed top-0 right-0 p-4">
+      <ul className="flex space-x-10">
         <li>
-          <Link to="/contact" className="text-blue-500 hover:text-blue-700">Contact</Link>
+          <Link to="/contact" className="text-blue-500 text-2xl ">Contact</Link>
         </li>
         <li>
-          <Link to="/login" className="text-blue-500 hover:text-blue-700">Login</Link>
+          <Link to="/login" className="bg-blue-800 text-white px-8 py-4 rounded hover:bg-blue-900">Login</Link>
         </li>
       </ul>
     </nav>
