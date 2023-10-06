e24f387caa972a59fa7bbbd5f0ad49e41c3e4cd2
RickMohamedFiri <rick.mohamed@student.moringaschool.com>
2023-10-06 13:23:28 +0300

    menu styling

--- client/src/components/menu.js
+++ client/src/components/menu.js
@@ -5,8 +5,8 @@ function Menu(){
         <div className="bg-blue-400 p-4 w-3/12 border-rounded h-screen">
             <h1 className="text-4xl font-extrabold font-sans text-cyan-900 pb-6">LATTO EXPENSE TRACKER</h1>
              <div className="flex flex-col ">
-             <Link to={"/homepage"} className="rounded-md active:bg-blue-700 text-cyan-900 font-bold p-4 text-2xl hover:text-3xl">HOME</Link>
-             <Link to={"/analysis"}className="rounded-md active:bg-blue-700 text-cyan-900 p-4 font-bold text-2xl hover:text-3xl">ANALYSIS</Link>
+             <Link to={"/homepage"} className="rounded-md p-4 text-2xl hover:bg-blue-700 hover:text-White">HOME</Link>
+             <Link to={"/analysis"}className="rounded-md p-4 text-2xl hover:bg-blue-700 hover:text-White">ANALYSIS</Link>
              </div>
              <br/><br/><br/><br/><br/><br/>
              <Link to={"/expenseform"} className="bg-blue-200 rounded-md p-2 hover:h-12 hover:text-lg ml-36">ADD EXPENSE</Link>
